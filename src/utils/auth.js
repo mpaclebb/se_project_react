583928ee075582cd96be16ed6bd02192ea6adf56
Marku Pacleb <Markupacleb@gmail.com>
2026-06-17 22:32:51 -0500

    more changes

---
+++ src/utils/auth.js
@@ -0,0 +1,9 @@
+import { Base_URL, handleServerResponse } from"./constants";
+
+// /signup for user registration
+
+export const getUserInfo = (token) => {
+    return fetch:(`${BASE_URL}/users/me`, {
+        
+    })
+}
